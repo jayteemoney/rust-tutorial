88da9c377690ba2538c58f2a74dec078da4caf4a
jayteemoney <jethroirmiya@gmail.com>
2025-05-19 13:18:18 +0000

    match statements

--- src/main.rs
+++ src/main.rs
@@ -64,6 +64,17 @@ fn main() {
     println!("Static: {}", greeting);
     println!("Dynamic: {}", dynamic_greeting);
 
+    // MATCH STATEMENTS AND EXAMPLES
+    // EXAMPLE1
+    let number = 3;
+    match number {
+      1 => println!("one!"),
+      2 => println!("two!"),
+      3 => println!("three!"),
+      _=> println!("something else!"),
+
+    }
+
 
 }
 
